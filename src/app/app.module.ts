6d489d3d945c427451244598584db9ecbbdc4355
Florian Popa <florian@webgenerals.com>
2017-05-28 18:43:06 +0200

    Add first module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FirstModule } from './first-module/first.module';
 
 @NgModule({
   declarations: [
@@ -11,10 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FirstModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
